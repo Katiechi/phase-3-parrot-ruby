e75200c8d6f2d0b4cc0fbf123251673b3253be68
brian ambundo <brian.katiechi9@gmail.com>
2022-08-18 14:41:47 +0300

    commit tree 33354ff1b1835962d776382ca09db2096a4c06ec

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot (sound= "Squawk!" )
+    if sound == "Squawk!"
+        puts "Squawk!"
+      return "Squawk!"
+    else
+        puts "Pretty bird!"
+      return "Pretty bird!"
+    end
+    
+ end
+ puts parrot()
